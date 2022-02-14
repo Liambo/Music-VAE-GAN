181fd9fc90a5dd3019b9c1443c5ca021a2ea0ce4
Liam Boyd <68342094+Liambo@users.noreply.github.com>
2022-02-14 19:33:51 +0000

    Improved data loader to load entire dataset & split into train/test sets, set up network to handle start & stop tokens.

--- VAE.py
+++ VAE.py
@@ -1,3 +1,4 @@
+from statistics import mean
 import torch
 from torch import nn, optim
 from torch.autograd import Variable
@@ -5,10 +6,13 @@ import numpy as np
 import os
 import pypianoroll
 import datetime
+import pretty_midi as pm
 
+# consider bidirectional, start notes have more importance on 2nd pass
 
-def load_batch(genre, batch_size, low_note, high_note):
+def load_dataset(genre, batch_size=64, low_note=20, high_note=104, train_split=0.9):
     directory = os.getcwd() + '/Dataset/Genres/' + genre
+    dataset = []
     batch = []
     try:
         for file in os.listdir(directory):
@@ -25,39 +29,72 @@ def load_batch(genre, batch_size, low_note, high_note):
                     songroll[i] = empty
             songroll = np.array(songroll) # Convert batch to numpy array
             # Concatenate all separate track vectors into a single vector
-            songroll = np.concatenate((songroll[0], songroll[1], songroll[2],songroll[3], songroll[4]), axis = 1)
+            flags = np.zeros((songroll.shape[1], 2))
+            songroll = np.concatenate((flags, songroll[0], songroll[1], songroll[2],songroll[3], songroll[4]), axis = 1)
+            start = np.expand_dims(np.zeros(422), 0)
+            start[0][0] = 1
+            end = np.expand_dims(np.zeros(422), 0)
+            end[0][1] = 1
+            songroll = np.concatenate((start, songroll, end))
             for i in range(songroll.shape[0]//96): # Split song up into 4 beat segments i.e. 1 bar 
                 batch.append(np.array(songroll[96*i:96*(i+1), :]))
                 if len(batch) >= batch_size:
-                    break
-            print('loaded {} of {}'.format(len(batch), batch_size))
-            if len(batch) >= batch_size:
-                break
+                    dataset.append(np.transpose(np.array(batch), (1, 0, 2)).astype('float32')) # Transpose so batch is 2nd dimension
+                    batch = []
     except FileNotFoundError:
         print('Error: no such genre as', genre)
         return
-    
-    return np.transpose(np.array(batch), (1, 0, 2)).astype('float32') # Tranpose so batch is 2nd dimension DECIDE IF WORKS
+    split_point = int(len(dataset)*train_split)
+    print(split_point, 'batches in train set,', len(dataset) - split_point, 'batches in test set.')
+    return np.array(dataset[:split_point]), np.array(dataset[split_point:])
 
 
-def convert_batch(batch, genre, path, low_note, high_note):
-    filename = path + '/' + genre + '/' + str(datetime.datetime.now())
+def convert_batch(batch, low_note=20, high_note=104, threshold=0.5): # Converts entire batch to 1 long song, or converts until stop token found.
     note_range = high_note - low_note
-    pianoroll = [[]] * 5
-    print(pianoroll)
+    pianoroll = [[] for _ in range(5)]
     batch = batch.tolist()
     for i in range(len(batch[0])):
         for j in range(len(batch)):
+            if batch[j][i][0] == 1:
+                continue
+            if batch[j][i][1] == 1:
+                return pianoroll
             for k in range(5):
-                noteslist = batch[j][k][k*note_range:(k+1)*note_range]
+                noteslist = batch[j][i][k*note_range+2:(k+1)*note_range+2]
                 for l in range(len(noteslist)):
-                    if noteslist[l] >= 0.5:
+                    if noteslist[l] >= threshold:
                         noteslist[l] = 1
                     else:
                         noteslist[l] = 0
                 pianoroll[k].append([0]*low_note + noteslist + [0]*(128-high_note))
     return pianoroll   
 
+def convert_pianoroll(pianoroll, tempo=120): # Converts a pianoroll in a list to a prettymidi object to be saved as a MIDI file
+    note_time = 60/(tempo*24)
+    drums = pm.Instrument(program=0, is_drum=True, name="drums") # Tempo is 120 here by default
+    piano = pm.Instrument(program=0, name="piano")
+    guitar = pm.Instrument(program=24, name="guitar")
+    bass = pm.Instrument(program=32, name="bass")
+    strings = pm.Instrument(program=48, name="strings")
+    for i in range(len(pianoroll[0])):
+        for j in range(128):
+            if pianoroll[0][i][j] == 1:
+                drums.notes.append(pm.Note(velocity=100, pitch=j, start=i*note_time, end=(i+1)*note_time))
+            if pianoroll[1][i][j] == 1:
+                piano.notes.append(pm.Note(velocity=100, pitch=j, start=i*note_time, end=(i+1)*note_time))
+            if pianoroll[2][i][j] == 1:
+                guitar.notes.append(pm.Note(velocity=100, pitch=j, start=i*note_time, end=(i+1)*note_time))
+            if pianoroll[3][i][j] == 1:
+                bass.notes.append(pm.Note(velocity=100, pitch=j, start=i*note_time, end=(i+1)*note_time))
+            if pianoroll[4][i][j] == 1:
+                strings.notes.append(pm.Note(velocity=100, pitch=j, start=i*note_time, end=(i+1)*note_time))
+    song = pm.PrettyMIDI(initial_tempo=tempo)
+    song.instruments.append(drums)
+    song.instruments.append(piano)
+    song.instruments.append(guitar)
+    song.instruments.append(bass)
+    song.instruments.append(strings)
+    return song
 
 class VAE(nn.Module):
     def __init__(self, input_size, hidden_dim, latent_dim):
@@ -73,6 +110,8 @@ class VAE(nn.Module):
             hidden_size=self.hidden_dim)
             self.decoder = nn.GRUCell(input_size=self.input_size,
             hidden_size=self.hidden_dim)
+            self.discriminator = nn.GRUCell(input_size=self.input_size,
+            hidden_size=self.hidden_dim)
             self.fc_mu = nn.Linear(in_features=self.hidden_dim,
             out_features=self.latent_dim)
             self.fc_logvar = nn.Linear(in_features=self.hidden_dim,
@@ -81,6 +120,8 @@ class VAE(nn.Module):
             out_features=self.hidden_dim)
             self.fc_decoder = nn.Linear(in_features=self.hidden_dim,
             out_features=self.input_size)
+            self.fc_discriminator = nn.Linear(in_features=self.hidden_dim,
+            out_features=2)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
@@ -102,7 +143,7 @@ class VAE(nn.Module):
             hidden = self.decoder(output_note, hidden)
             output_note = self.fc_decoder(hidden)
             output = torch.cat((output, torch.unsqueeze(output_note, 0)), 0)
-        return output, mu, logvar
+        return output, mu, logvar, z
 
 
 class Optimisation:
@@ -113,39 +154,69 @@ class Optimisation:
         self.val_losses = []
         self.mse = nn.MSELoss()
         self.beta = beta
+        self.genredict = {"Blues": 0, "Country": 1, "Jazz": 2, "Rock": 3, "Pop": 4}
 
-    def loss_fn(self, x, x_hat, mu, logvar):
+    def loss_fn(self, x, x_hat, mu, logvar, z, genre):
         mse_loss = self.mse(x, x_hat) # reconstruction loss between input & output, i.e. how similar are they
         kl_div = (0.5 * (mu ** 2 + torch.exp(logvar) - logvar - 1).sum(axis=1)).mean()
         # KL Div loss, i.e. how similar is prior to posterior distribution. We sum loss over latent variables for each batch & timestep, then mean.
+        
         return mse_loss + self.beta * kl_div
     
-    def train_step(self, x):
+    def train_step(self, x, genre, vae=True):
+        self.optimiser.zero_grad()
         self.model.train()
-        x_hat, mu, logvar = self.model(x)
-        loss = self.loss_fn(x, x_hat, mu, logvar)
+        if vae:
+            x_hat, mu, logvar, z = self.model(x)
+            loss = self.loss_fn(x, x_hat, mu, logvar, z, genre)
         loss.backward()
         self.optimiser.step()
-        self.optimiser.zero_grad()
         return loss.item()
     
-    def train(self, train_set, eval_set, n_epochs=50):
-        train_losses = []
+    def test_step(self, x, genre):
+        x_hat, mu, logvar, z = self.model(x)
+        loss = self.loss_fn(x, x_hat, mu, logvar, z, genre)
+        return loss.item()
+    
+    def train(self, train_set, test_set, device, n_epochs=50, eval_every=5, genre='Jazz'):
         for epoch in range(1, n_epochs+1):
-            train_set = torch.from_numpy(load_batch('Jazz', 64, 20, 104))
-            loss = self.train_step(train_set)
-            train_losses.append(loss)
+            if epoch % eval_every == 0:
+                eval_losses = []
+                for batch in test_set:
+                    input = torch.from_numpy(batch).to(device=device)
+                    loss = self.test_step(input, genre)
+                    eval_losses.append(loss)
+                print('losses for epoch', epoch)
+                print(eval_losses)
+                print('mean loss: ' + mean(eval_losses))
+            for batch in train_set:
+                input = torch.from_numpy(batch).to(device=device)
+                self.train_step(input, genre)
+                print('done batch')
             print('done epoch {} of {}'.format(epoch, n_epochs))
         print(train_losses)
 
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+path = os.getcwd()
+genre = "jazz"
 learning_rate = 0.001
 weight_decay = 0.000001
-batch = load_batch('Jazz', 64, 20, 104)
-pianoroll = convert_batch(batch, 'none', 'none', 20, 104)
-input_tensor = torch.from_numpy(batch)
+train_set, test_set = load_dataset('Jazz', 64, 20, 104)
+song = convert_batch(train_set[0])
+song = convert_pianoroll(song)
+save_path = path + '/' + genre + '/' + str(datetime.datetime.now()) + '.mid'
+song.write(save_path)
+input_tensor = torch.from_numpy(train_set[0]).to(device=device)
 input_tensor = input_tensor.type(torch.FloatTensor)
-vae = VAE(420, 128, 128)
+vae = VAE(422, 128, 128)
 optimiser = Optimisation(vae, optim.Adam(vae.parameters(), lr=learning_rate, weight_decay=weight_decay), 1.0)
-optimiser.train(None, None, 10)
-output_tensor, _, _ = vae(input_tensor)
-print(convert_batch(output_tensor, 'none', 'none', 20, 104))
+optimiser.train(train_set, test_set, device, 10)
+output_tensor, _, _, _ = vae(input_tensor)
+batch = convert_batch(output_tensor)
+song = convert_pianoroll(batch)
+save_path = path + '/' + genre + '/' + str(datetime.datetime.now()) + '.mid'
+song.write(save_path)
